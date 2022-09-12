5fb00694d8646c976d387a242b663151afd5cae2
ichangai gitundu <ichangaigitundu@gmail.com>
2022-09-12 23:38:13 +0300

    Configured Amazon-S3

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,65 @@
+const router = require('express').Router()
+const Product = require("../models/product")
+
+const upload = require("../middlewares/upload-photo")
+
 // post request = create a new product
 
+router.post("/products", upload.single("photo"), async (req, res) => {
+    try{
+       let product = new Product();
+       product.title = req.body.title;
+       product.description = req.body.description;
+       product.photo = req.file.location;
+       product.price = req.body.price;
+       product.stockQuantity = req.body.stockQuantity;
+    //    product.rating = req.body.rating;
 
+       await product.save() //async
+
+       res.json({
+        status: true,
+        message: "Successfully saved"
+       });
+
+    }catch(err){
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+})
 // Get requets - get all products
+
+router.get('/products', async (req, res) => {
+    try{
+       let product = new Product();
+       product.title = req.body.title;
+       product.description = req.body.description;
+       product.photo = req.body.photo;
+       product.price = req.body.price;
+       product.stockQuantity = req.body.stockQuantity;
+
+       await product.save()
+
+       res.json({
+        status: true,
+        message: "Successfully saved"
+       })
+
+    }catch(err){
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+})
 
 // Get request -  get a single product
 
 // PUT request - update a single product
 
-// 
+//Delete request - delete a single product
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -9,8 +9,6 @@ const User = require('./models/user');
 
 dotenv.config();
 require('dotenv').config()
-// dotenv.config({ path: ".env" });
-// connectDB() 
 const app = express()
 
 
@@ -19,26 +17,10 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended :false}));
 
+// require apis
+const productRoutes = require('./routes/products');
 
-app.post("/", (req, res) => {
- let user = new User();
-    user.name = req.body.name;
-    user.email = req.body.email;
-    user.password = req.body.password;
-
-    user.save((err) => {
-        if(err){
-            res.json(err); 
-        }else {
-            res.json('successfully saved')
-        }
-    })
-
-})
-
-app.get('/', (req, res) => {
-    res.json("Amazing Amazon Purpose")
-})
+app.use("/api", productRoutes);
 
 app.listen(3000, err => {
     if(err){
